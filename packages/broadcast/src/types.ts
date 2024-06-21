8949f709cade8b7814f9cd0ce7a7ed3c2121afd6
Alex Risch <alexrisch@Alexs-MacBook-Pro-2.local>
2024-06-21 15:23:48 -0600

    chore: Update readme and js docs
    Updated docs to be found on the interface as well as class
    Updated ReadMe for better explanations and overview of broadcast lifecycle

--- packages/broadcast/src/types.ts
+++ packages/broadcast/src/types.ts
@@ -8,15 +8,41 @@ export interface BroadcastConstructorParams<ContentTypes = unknown> {
   rateLimitTime?: number
 
   // Callbacks
+  /**
+   * Called when a batch of addresses is about to be sent
+   */
   onBatchStart?: (addresses: string[]) => void
+  /**
+   * Called when a batch of addresses has been sent/failed
+   */
   onBatchComplete?: (addresses: string[]) => void
+  /**
+   * Called when all addresses have been sent/failed
+   */
   onBroadcastComplete?: () => void
+  /**
+   * Called when an address can't be messaged
+   */
   onCantMessageAddress?: (address: string) => void
-  onCanMessageAddreses?: (addresses: string[]) => void
-  onMessageSending?: (address: string) => void
-  onMessageFailed?: (address: string) => void
-  onMessageSent?: (address: string) => void
+  /**
+   * Called when the list of addresses that can be messaged is updated, this is useful for caching
+   */
   onCanMessageAddressesUpdate?: (addresses: string[]) => void
+  /**
+   * Called when a message is about to be sent
+   */
+  onMessageSending?: (address: string) => void
+  /**
+   * Called when a message fails to send
+   */
+  onMessageFailed?: (address: string) => void
+  /**
+   * Called when a message is successfully sent
+   */
+  onMessageSent?: (address: string) => void
+  /**
+   * Called when a delay is about to happen
+   */
   onDelay?: (ms: number) => void
 }
 
