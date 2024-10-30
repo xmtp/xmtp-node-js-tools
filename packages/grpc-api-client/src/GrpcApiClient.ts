3be7274b5ce5f6f47f8d84596430c6d0439ff498
Andrew Plaza <github@andrewplaza.dev>
2024-10-30 13:43:38 -0400

    make sure to add err to other callback as well

--- packages/grpc-api-client/src/GrpcApiClient.ts
+++ packages/grpc-api-client/src/GrpcApiClient.ts
@@ -250,7 +250,7 @@ export default class GrpcApiClient implements ApiClient {
           if (new Date().getTime() - startTime.getTime() < 1000) {
             await sleep(1000)
           }
-          onConnectionLost?.()
+          onConnectionLost?.(e)
           this.logger.error({ error: e }, "stream error")
         }
       }
